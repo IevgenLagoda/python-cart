4cb37195718692c0ba680fc3a0a56a95b8a54bcb
Denis Vasin <svga.shop@gmail.com>
2022-05-16 11:49:44 +0300

    https://github.com/IevgenLagoda/python-cart ''

--- productcart_test.py
+++ productcart_test.py
@@ -3,27 +3,41 @@ from product import Product
 from productcart import ProductCart
 
 class TestProductCart(unittest.TestCase):
-    def test_get_data(self):
+    def test_get_product(self):
         product = Product(1, 'test', 100)
-        amount = 2
-        discount = 20
-        total_price = 180
-        productcart = ProductCart(product, amount, discount)
-        self.assertEqual(productcart.getProduct(), product)
+        productcart = ProductCart(product,  2, 20)
         # TODO: to use constants as 2nd params.
-        self.assertEqual(productcart.getAmount(), 2)
-        self.assertEqual(productcart.getDiscount(), 20)
-        # TODO: to use total_price as 2nd pram?
-        self.assertEqual(productcart.getProductTotalPrice(), 180)
+        self.assertEqual(productcart.getProduct(), product)
 
-    def test_set_data(self):
+    def test_get_amount(self):
+        amount = 2
+        productcart = ProductCart(Product(1, 'test', 100), amount, 20)
+        self.assertEqual(productcart.getAmount(), amount)
+
+    def test_get_discount(self):
+        discount = 20
+        productcart = ProductCart(Product (1, 'test', 100), 2, discount)
+        self.assertEqual(productcart.getDiscount(), discount)
+
+    def test_get_totalprice(self):
+        total_price = 180
+        productcart = ProductCart (Product (1, 'test', 100), 2, 20)
+        # TODO: to use total_price as 2nd pram?
+        self.assertEqual(productcart.getProductTotalPrice(), total_price)
+
+    def test_set_amount(self):
         productcart = ProductCart(Product(1, 'test', 100), 2, 20)
         new_amount = 3
-        new_discount = 10
         # TODO: to check for the old values first.
+        self.assertEqual (productcart.getAmount(), 2)
         productcart.setAmount(new_amount)
+        self.assertEqual(productcart.getAmount(), new_amount)
+
+    def test_set_discount(self):
+        productcart = ProductCart (Product (1, 'test', 100), 2, 20)
+        new_discount = 10
+        self.assertEqual (productcart.getDiscount(), 20)
         productcart.setDiscount(new_discount)
-        self.assertEqual(productcart.getAmount(), 3)
-        self.assertEqual(productcart.getDiscount(), 10)
+        self.assertEqual(productcart.getDiscount(), new_discount)
 
 unittest.main()
