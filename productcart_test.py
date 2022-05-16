36efb5abc5f87dc655d413213c3cdf346d5df74f
Denis Vasin <svga.shop@gmail.com>
2022-05-16 19:24:17 +0300

    merge branch 'main' of https://github.com/IevgenLagoda/python-cart ""

--- productcart_test.py
+++ productcart_test.py
@@ -16,7 +16,7 @@ class TestProductCart(unittest.TestCase):
 
     def test_get_discount(self):
         discount = 20
-        productcart = ProductCart(Product (1, 'test', 100), 2, discount)
+        productcart = ProductCart(Product(1, 'test', 100), 2, discount)
         self.assertEqual(productcart.getDiscount(), discount)
 
     def test_get_totalprice(self):
@@ -29,14 +29,14 @@ class TestProductCart(unittest.TestCase):
         productcart = ProductCart(Product(1, 'test', 100), 2, 20)
         new_amount = 3
         # TODO: to check for the old values first.
-        self.assertEqual (productcart.getAmount(), 2)
+        self.assertEqual(productcart.getAmount(), 2)
         productcart.setAmount(new_amount)
         self.assertEqual(productcart.getAmount(), new_amount)
 
     def test_set_discount(self):
         productcart = ProductCart (Product (1, 'test', 100), 2, 20)
         new_discount = 10
-        self.assertEqual (productcart.getDiscount(), 20)
+        self.assertEqual(productcart.getDiscount(), 20)
         productcart.setDiscount(new_discount)
         self.assertEqual(productcart.getDiscount(), new_discount)
 
