26daef838c34c648c7046054b3afb808710b3aeb
Denis Vasin <svga.shop@gmail.com>
2022-05-16 12:39:07 +0300

    https://github.com/IevgenLagoda/python-cart ''

--- cart.py
+++ cart.py
@@ -12,20 +12,26 @@ class Cart:
     # TODO: should be able to call as:
     # TODO: addProductToCart(nw Product(), 2, 10)
     # TODO: addProductToCart(nw ProductCart())
-    def addProductToCart(self, product):
+    def addProductToCart(self, product, amount=1, discount=0):
         if isinstance(product, ProductCart):
-            id = product.getId()
             # TODO: getAmount() ?
-            if product.getAmount == 0:
+            if product.getAmount() == 0:
                 return
+            id = product.getId()
             if id not in self.products:
                 self.products[id] = product
             else:
-                self.products[id].setAmount(self.products[id].getAmount() + product.getAmount)
+                self.products[id].setAmount(self.products[id].getAmount() + product.getAmount())
             self.products[id].setDiscount(product.getDiscount())
-        else:
-            product, amount, discount = product
-            self.addProductToCart(ProductCart(product, amount, discount))
+        elif isinstance(product, Product):
+            if amount == 0:
+                return
+            id = product.getId()
+            if id not in self.products:
+                self.products[id] = ProductCart(product, amount, discount)
+            else:
+                self.products[id].setAmount(self.products[id].getAmount() + amount)
+            self.products[id].setDiscount(discount)
 
     def isUserValid(self):
         return self.user.isUserDataExists()
@@ -65,19 +71,3 @@ class Cart:
         return self.products[id].getAmount()
 
 # TODO: move all the cases into the unittest and remove the section.
-if __name__ == "__main__":
-    cartUser = User("Den", "Vasin", "0503616655", "Fesenko 1", "den2001@ukr.net", "qwerty")
-    product1 = Product(1, 'notebook HP', 400)
-    product2 = Product(2, 'notebook Acer', 350)
-
-    cart = Cart(cartUser)
-    productCart1 = ProductCart(product1, 1, 10)
-    productCart2 = ProductCart(product2, 2, 20)
-    cart.addProductToCart((product1, 0, 10))
-    cart.addProductToCart((product2, 4, 20))
-
-    print(cart.getTotalCartPrice())
-    print(cart.removeProductById(2, 2))
-    print(cart.isUserValid())
-    print(cart.isUserAuthValid("den2001@ukr.net", "qwerty"))
-    print(cart.getTotalDicount())
